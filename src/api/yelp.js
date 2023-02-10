447eb508ed9110d460521cd1696ade140b2f3bd9
Raphael Chukwuka Ugwu <ugwuraphael@gmail.com>
2023-02-10 16:50:16 +0000

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer bXXBcBEUzxHSO4Zpx1zv-cMGH83hV-CUZbkhcEJG2L9Sv7cJBGUPme2N74hxPoBz9vgD_abJ0g7-e9juFhjw0fuCqk743PBt_MU9Kn5_R99rntGIOvU7CCUdfiDmY3Yx'
+        Authorization: 'Bearer INSERT-API-KEY-HERE'
     }
 })
