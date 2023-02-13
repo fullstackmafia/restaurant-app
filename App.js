91a1498945dca25d8f440ab0e54e4b3953e47747
Raphael Ugwu <ugwuraphael@gmail.com>
2023-02-13 21:24:14 +0000

    updated readme file

--- App.js
+++ App.js
@@ -3,24 +3,12 @@ import { createStackNavigator } from "@react-navigation/stack";
 import SearchScreen from "./src/screens/SearchScreen";
 import ShowResultsScreen from "./src/components/ShowResultsScreen";
 
-// const navigator = createStackNavigator({
-//   Search: SearchScreen,
-//   ShowResults: ShowResultsScreen
-// }, {
-//   initialRouteName: 'Search',
-//   defaultNavigationOptions: {
-//     title: 'Business Search'
-//   }
-// });
-
-// export default createAppContainer(navigator);
-
 const Stack = createStackNavigator();
 
 export default function App() {
   return(
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerTitle: " Business Search"}}>
+      <Stack.Navigator screenOptions={{ headerTitle: "Restaurant Search"}}>
         <Stack.Screen name="Search" component={SearchScreen} />
         <Stack.Screen name="ShowResults" component={ShowResultsScreen} />
       </Stack.Navigator>
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# restaurant-app
+# Restaurant app built with React Native, Hooks, and Axios
+
+This is a basic restaurant app demo built with React Native, Hooks, Yelp API, and Axios to learn about React Native.
+
+## Screens and Features
+
+&nbsp;
+
+### Home Screen
+
+This screen shows a search bar and a collection of restaurants.
+
+![jkn](https://res.cloudinary.com/fullstackmafia/image/upload/v1676323112/pic-1_imb9ia.jpg)
+
+### Restaurant Overview
+
+This screen shows a collection of pictures for a selected restaurant (for now!)
+
+![knjk](https://res.cloudinary.com/fullstackmafia/image/upload/v1676323112/pic-2_r3jntl.jpg)
+
+To install/run:
+
+$ git clone https://github.com/fullstackmafia/restaurant-app.git
+$ cd restaurant-app
+$ npm install
+$ expo start --web or expo start
